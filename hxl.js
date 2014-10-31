e57acee9615a6a589619ce46ff663cfc2cdbe609
David Megginson <david.megginson@megginson.com>
2014-10-30 21:12:15 -0400

    Added simple unit tests.
    Added default values for constructors.

--- hxl.js
+++ hxl.js
@@ -6,6 +6,14 @@
  * Documentation: http://hxlstandard.org
  */
 
+function _default_arg(value, default_value) {
+    if (typeof(value) === 'undefined') {
+        return default_value;
+    } else {
+        return value;
+    }
+}
+
 /**
  * A HXL dataset
  */
@@ -13,30 +21,26 @@ function HXLDataset(url) {
     this.url = url;
     this.columns = [];
     this.rows = [];
-    if (url) {
-        // TODO load from URL
-        // TODO maybe also accept list of data
-    }
 }
 
 /**
  * A HXL column definition.
  */
-function HXLColumn() {
-    this.headerString = [];
-    this.hxlTag = [];
-    this.lang = [];
-    this.columnNumber = -1;
-    this.sourceColumnNumber = -1;
+function HXLColumn(hxlTag, lang, headerString, columnNumber, sourceColumnNumber) {
+    this.hxlTag = _default_arg(hxlTag, null);
+    this.lang = _default_arg(lang, null);
+    this.headerString = _default_arg(headerString, null);
+    this.columnNumber = _default_arg(columnNumber, -1);
+    this.sourceColumnNumber = _default_arg(sourceColumnNumber, -1);
 }
 
 /**
  * A row of HXL data.
  */
-function HXLRow() {
-    this.values = [];
-    this.rowNumber = -1;
-    this.sourceRowNumber = -1;
+function HXLRow(rowNumber, sourceRowNumber, values) {
+    this.rowNumber = _default_arg(rowNumber, -1);
+    this.sourceRowNumber = _default_arg(sourceRowNumber, -1)
+    this.values = _default_arg(values, []);
 }
 
 /**
---
+++ test/tests.js
@@ -0,0 +1,64 @@
+
+// HXLDataset tests
+
+QUnit.test("create dataset", function(assert) {
+    var dataset = new HXLDataset();
+    assert.ok(dataset, "created dataset");
+    assert.equal(dataset.url, null, "dataset.url defaults to null");
+});
+
+QUnit.test("dataset constructor params", function(assert) {
+    var url = 'http://example.org/dataset.csv';
+
+    var dataset = new HXLDataset(url);
+    assert.equal(dataset.url, url, "dataset.url assigned in constructor");
+});
+
+// HXLColumn tests
+
+QUnit.test("create column", function(assert) {
+    var column = new HXLColumn();
+    assert.ok(column, "created column");
+    assert.equal(column.hxlTag, null, "column.hxlTag defaults to null");
+    assert.equal(column.lang, null, "column.lang defaults to null");
+    assert.equal(column.headerString, null, "column.headerString defaults to null");
+    assert.equal(column.columnNumber, -1, "column.columnNumber defaults to -1");
+    assert.equal(column.sourceColumnNumber, -1, "column.sourceColumnNumber defaults to -1");
+});
+
+QUnit.test("column constructor params", function(assert) {
+    var hxlTag = "#org";
+    var lang = "fr";
+    var headerString = "Organisation";
+    var columnNumber = 5;
+    var sourceColumnNumber = 4;
+
+    var column = new HXLColumn(hxlTag, lang, headerString, columnNumber, sourceColumnNumber);
+    assert.equal(column.hxlTag, hxlTag, "column.hxlTag assigned in constructor");
+    assert.equal(column.lang, lang, "column.lang assigned in constructor");
+    assert.equal(column.headerString, headerString, "column.headerString assigned in constructor");
+    assert.equal(column.columnNumber, columnNumber, "colum.columnNumber assigned in constructor");
+    assert.equal(column.sourceColumnNumber, sourceColumnNumber, "colum.sourceColumnNumber assigned in constructor");
+});
+
+// HXLRow tests
+
+QUnit.test("create row", function(assert) {
+    var row = new HXLRow();
+    assert.ok(row, "created row");
+    assert.equal(row.rowNumber, -1, "row.rowNumber defaults to -1");
+    assert.equal(row.sourceRowNumber, -1, "row.sourceRowNumber defaults to -1");
+    assert.deepEqual(row.values, [], "row.values defaults to []");
+});
+
+QUnit.test("row constructor params", function(assert) {
+    var rowNumber = 5;
+    var sourceRowNumber = 7;
+    var values = ['a', 'b', 'c'];
+
+    var row = new HXLRow(rowNumber, sourceRowNumber, values);
+    assert.ok(row, "created row");
+    assert.equal(row.rowNumber, rowNumber, "row.rowNumber defaults to -1");
+    assert.equal(row.sourceRowNumber, sourceRowNumber, "row.sourceRowNumber defaults to -1");
+    assert.deepEqual(row.values, values, "row.values defaults to []");
+});
